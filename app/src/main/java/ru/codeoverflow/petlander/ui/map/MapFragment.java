692c3cbb1778d568e2da25250c1e78d2b27c2e85
Rand1kYanovich <rand1kch@gmail.com>
2019-04-05 22:29:31 +0400

    Add in MapFragment some marker from FireBase

--- app/src/main/java/ru/codeoverflow/petlander/ui/map/MapFragment.java
+++ app/src/main/java/ru/codeoverflow/petlander/ui/map/MapFragment.java
@@ -3,6 +3,7 @@ package ru.codeoverflow.petlander.ui.map;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,9 +11,18 @@ import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import ru.codeoverflow.petlander.MainActivity;
+import ru.codeoverflow.petlander.ui.Cards.Cards;
 import ru.codeoverflow.petlander.ui.base.BaseFragment;
 import ru.codeoverflow.petlander.R;
 
@@ -20,6 +30,9 @@ public class MapFragment extends BaseFragment implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private MapView mapView;
+    protected Long geoX;
+    protected Long geoY;
+
 
     public MapFragment(){
 
@@ -41,6 +54,7 @@ public class MapFragment extends BaseFragment implements OnMapReadyCallback {
         mapView.onResume();
         mapView.getMapAsync(this);
 
+
     }
 
 
@@ -53,17 +67,38 @@ public class MapFragment extends BaseFragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        DatabaseReference userDb = FirebaseDatabase.getInstance().getReference().child("Users");
+        userDb.addListenerForSingleValueEvent(new ValueEventListener() {
+                                         @Override
+                                         public void onDataChange(DataSnapshot dataSnapshot) {
+                                                 if (dataSnapshot.exists()) {
+                                                     Toast.makeText(getContext(), "Hi", Toast.LENGTH_SHORT).show();
+                                                     for (DataSnapshot data : dataSnapshot.getChildren()) {
+
+
+                                                         geoX = (Long) data.child("geoX").getValue();
+                                                         Toast.makeText(getContext(), ""+geoX, Toast.LENGTH_SHORT).show();
+                                                         geoY = (Long) data.child("geoY").getValue();
+                                                         Toast.makeText(getContext(), ""+geoY, Toast.LENGTH_SHORT).show();
+
+                                                         LatLng position = new LatLng(geoX, geoY);
+
+
+                                                         mMap.addMarker(new MarkerOptions().position(position).title("Marker in Sydney"));
+                                                         Log.e("MAP", "OK");
+                                                 }
+                                             }
+                                         }
+
+                                         @Override
+                                         public void onCancelled(@NonNull DatabaseError databaseError) { }
+                                     }
+        );
 
         // Add a marker in Sydney and move the camera
 
-        LatLng sydney = new LatLng(-34,50);
 
-
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        Log.e("MAP","OK");
 
 
     }
-
 }
